28b5d7a5717a07140fd1704b0a7a1f7a5ce828a0
sudhadevopsaws <sudhaguddanti@gmail.com>
2023-11-20 22:51:05 +0530

    stash updated

--- chandu.sh
+++ chandu.sh
@@ -3,3 +3,4 @@ i am modified this file.
 hi this file woned to chandu
 i am updated in devlopment branch code
 i am upadted
+i am updating the code for production environment..
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-this is the development branch..
+this is the development branch. .
 again i am updated the code in the master branch
 i am updating the dev code.
+i have deploy the code in the dev env..
